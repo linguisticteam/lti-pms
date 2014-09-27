c8db31718dc4577ba79e83c571d7756dda570f54
Borislav Zlatanov <borislav_zlatanov@yahoo.co.uk>
2014-09-28 02:32:38 +0300

    Update config.php
    Remove the base_url in order to let CodeInitor figure it out itself. This change is needed because site now supports both http and https. I tested the change on the Portal and it works fine.

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://members.linguisticteam.org/pms/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
